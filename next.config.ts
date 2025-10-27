8f4afc518b0cd1c6857a9829375e2e6924687a4c
Matthew Enarle <enarlem10@gmail.com>
2025-10-27 09:53:03 +0800

    chore: fic SEO redirect errors

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  trailingSlash: false,
   images: {
     domains: ['i.scdn.co', 'mosaic.scdn.co', 'image-cdn-ak.spotifycdn.com', 'image-cdn-fa.spotifycdn.com'],
     formats: ['image/avif', 'image/webp']
-  }
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Robots-Tag',
+            value: 'index, follow',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
