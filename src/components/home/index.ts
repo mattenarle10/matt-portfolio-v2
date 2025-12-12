95f77f7e928aedeb035946c09a6b5a0c82566fed
Matthew Enarle <enarlem10@gmail.com>
2025-12-13 01:12:23 +0800

    refactor: add barrel export for layout components

--- src/components/home/index.ts
+++ src/components/home/index.ts
@@ -1,5 +1,4 @@
 export { default as Hello } from "./hello"
 export { default as RecentMediumPosts } from "./medium"
-export { default as RecentSpotify } from "./spotify"
-export { default as RecentStrava } from "./strava"
-
+export { SpotifyRecentlyPlayed } from "./spotify"
+export { StravaActivity } from "./strava"
